99739365580cafde09af60937e36416de9985dfc
Robert Gędłek <robert.gedlek@inveox.com>
2021-06-28 08:45:46 +0200

    feat: added basic setup

--- src/test/java/com/inveox/NotesContainerTest.java
+++ src/test/java/com/inveox/NotesContainerTest.java
@@ -1,20 +1,6 @@
 package com.inveox;
 
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 class NotesContainerTest {
     //TODO implement tests
-
-    @Test
-    void shoudl() {
-        Note x = new Note("ala");
-        NotesContainer notesContainer = new NotesContainer(3);
-        notesContainer.addNote(x);
-      //  notesContainer.addNote(new Note("bela"));
-
-        Assertions.assertThat(notesContainer.containsNote(new Note("bela")))
-                .isEqualTo(true);
-    }
 }
